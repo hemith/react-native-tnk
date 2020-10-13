cc7f3a76b4c273ac0aa92fe8363e3a0e600d6a4c
hemith <like.sunshine@gmail.com>
2020-10-13 21:43:04 +0900

    npm publish v0.1.6 - support android

---
+++ android/src/main/java/com/reactnativetnk/TnkModule.java
@@ -0,0 +1,37 @@
+package com.reactnativetnk;
+
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
+
+import com.tnkfactory.ad.TnkSession;
+
+import android.util.Log;
+
+public class TnkModule extends ReactContextBaseJavaModule {
+
+    private final ReactApplicationContext reactContext;
+
+    public TnkModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "Tnk";
+    }
+
+    @ReactMethod
+    public void applicationStarted(String name) {
+      Log.d("tnkad", "applicationStarted. " + name);
+       TnkSession.applicationStarted(this.reactContext);
+    }
+
+    @ReactMethod
+    public void actionCompleted(String actionName) {
+    	 Log.d("tnkad", "actionCompleted. " + actionName);
+       TnkSession.actionCompleted(this.reactContext, actionName);
+    }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import { NativeModules } from 'react-native';
 type TnkType = {
   multiply(a: number, b: number): Promise<number>;
   addEvent(a: string, b: string): void;
+  applicationStarted(a: string): void;
+  actionCompleted(a: string): void;
 };
 
 const { Tnk } = NativeModules;
